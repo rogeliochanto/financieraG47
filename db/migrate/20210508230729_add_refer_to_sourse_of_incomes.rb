510e1628b0fc6bcf24f7364f825254c2a496211c
rogelio <rogeliochanto@gmail.com>
2021-05-08 20:22:07 -0600

    varios arreglos en tabla

---
+++ db/migrate/20210508230729_add_refer_to_sourse_of_incomes.rb
@@ -0,0 +1,5 @@
+class AddReferToSourseOfIncomes < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :sourse_of_incomes, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_034557) do
+ActiveRecord::Schema.define(version: 2021_05_08_230729) do
 
   create_table "loans", force: :cascade do |t|
     t.integer "amount"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2021_05_08_034557) do
     t.boolean "approval"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sourse_of_incomes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 2021_05_08_034557) do
   end
 
   add_foreign_key "loans", "users"
+  add_foreign_key "sourse_of_incomes", "users"
 end
